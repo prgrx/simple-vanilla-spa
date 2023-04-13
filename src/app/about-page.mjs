c495915124e2f1e16b49d18909f280f587005189
Pedro Vargas <pedro@programmer.net>
2023-04-13 02:11:02 +0200

    lassst but not least

--- src/app/about-page.mjs
+++ src/app/about-page.mjs
@@ -7,7 +7,29 @@ export class AboutPage extends HTMLElement {
 	static selector = 'app-about'
 
 	connectedCallback() {
-		this.innerHTML = Array(100).fill('<p>We are a team of developers.</p>').join('')
+		this.innerHTML = `
+	<expense-tracker>
+			
+		<template id="expense-tracker">
+			<h2>Expense Tracker</h2>
+			<div>
+			  <label for="input-description">Description</label>
+			  <input id="input-description" type="text" />
+			</div>
+			<div>
+			  <label for="input-amount">Amount</label>
+			  <input id="input-amount" type="number" />
+			</div>
+			<button id="button-add">Add</button>
+			<div>
+			  <h3>Transaction History</h3>
+			  <ul id="transaction-list"></ul>
+			  <p>Balance: <span id="balance"></span></p>
+			</div>
+		  </template>
+	</expense-tracker>
+		`
+		//Array(100).fill('<p>We are a team of developers.</p>').join('')
         selectTab('/about')
 	}		
 
